550cdac39faceeb3a575c3950dec98444e4755d7
Dian Niu <zn15@duke.edu>
2019-08-09 20:01:53 -0400

    basic path reading for logging

--- logger.py
+++ logger.py
@@ -1,6 +1,19 @@
-#logging
+import json
+import sys
 
 class Logger:
 
     def __init__(self):
-        
+
+        assert len(sys.argv) > 1
+
+        self.path = None
+        for arg in sys.argv:
+            if "--config=" in arg:
+                firstsplit = arg.split("=")[1]
+                index = firstsplit.rfind("/")
+                self.path = firstsplit[:index] + "logs.json" #LOOK AT NAMING
+                break
+
+        assert self.path is not None
+        #print("The path is: " + self.path)
